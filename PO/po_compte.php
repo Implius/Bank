6d928bbea290fd15644bb388b1789ca2c2273ce9
GuillaumeAugeraud <augeraud.guillaume@gmail.com>
2024-10-03 14:32:57 +0200

    Update po_compte.php

--- PO/po_compte.php
+++ PO/po_compte.php
@@ -1,18 +1,60 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="po_compte.css">
-    <title>JeFinance</title>
-  </head>
-  <body>
-    <nav class="navbar">
-        <a class="onit" href="#">Compte</a> |
-        <a class="link" href="#">Remise</a> |
-        <a class="link" href="#">Impayé</a> |
-        <a class="link" href="#">Création</a> |
-        <a href="#">Deco</a>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="po.css">
+  <title>JeFinance</title>
+</head>
+
+<body>
+  <div class="navbar">
+    <nav>
+      <div class="onit">Compte</div>
+      <a class="link" href="#">Remise</a>
+      <a class="link" href="#">Impayé</a>
+      <a class="link" href="#">Création</a>
     </nav>
-  </body>
+    <a class="deco" href="#"></a>
+  </div>
+  <div class="Compte_tableau">
+    <table class="tableau">
+      <thead>
+        <tr>
+          <th class="table-blue">
+            Raison Social
+          </th>
+          <th class="table-green">
+            N° de compte
+          </th>
+          <th class="table-yellow">
+            N° SIREN
+          </th>
+          <th class="table-darkblue">
+            Montant du compte
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td class="white">
+            Picard
+          </td>
+          <td class="grey">
+            **** 1018
+          </td>
+          <td class="white">
+            784 939 688
+          </td>
+          <td class="grey montant">
+            87152.09 €
+          </td>
+        </tr>
+        <!--Mettre code PHP ici-->
+      </tbody>
+    </table>
+  </div>
+</body>
+
 </html>
