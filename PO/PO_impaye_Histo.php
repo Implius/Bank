51622b7f844a7b8d623d652aa834beab5c43b61b
GuillaumeAugeraud <augeraud.guillaume@gmail.com>
2024-10-10 14:02:31 +0200

    Update PO_impaye_Histo.php

--- PO/PO_impaye_Histo.php
+++ PO/PO_impaye_Histo.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="PO.css">
+  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
   <title>JeFinance</title>
   <script>
     function sortTable() {
@@ -17,81 +18,107 @@
 </head>
 
 <body>
-
-  <?php
-  $onit = "Impaye";
-  include("../include/po_navbar.inc.php"); // Navbar
-  ?>
-
-<div class="mini_navbar">
-      <a class="link" href="PO_impaye_tableau.php">Tableau</a>
-      <div class="onit">Histogramme</div>
-      <a class="link" href="PO_impaye_Circu.php">Circulaire</a>
+  <div class="navbar">
+    <nav>
+      <a class="link" href="PO_compte.html">Compte</a>
+      <a class="link" href="PO_remise.html">Remise</a>
+      <div class="onit">Impayé</div>
+      <a class="link" href="PO_creation.html">Création</a>
+    </nav>
+    <a class="deco" href="index.html"></a>
   </div>
-  <div class="Compte_tableau">
-    <div class="sorting">
-      Trier par :
-      <select name="sort_by" id="sort_by" onchange="sortTable()">
-        <option value="date_plusrecent">Date (plus récent)</option>
-        <option value="date_plusancient">Date (plus ancient)</option>
-        <option value="numero_remise">Numéro d'impayé</option>
-        <option value="Numero_SIREN">Numéro SIREN</option>
-      </select>
-    </div>
-    <table class="tableau">
-      <thead>
-        <tr>
-          <th class="table-blue">
-            Impayé N°
-          </th>
-          <th class="table-green">
-            Date
-          </th>
-          <th class="table-orange">
-            Autre parti
-          </th>
-          <th class="table-yellow">
-            N° SIREN Autre parti
-          </th>
-          <th class="table-red">
-            Objet
-          </th>
-          <th class="table-darkblue">
-            Raison
-          </th>
-          <th class="table-purple">
-            Montant
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td class="white">
-            0001
-          </td>
-          <td class="grey">
-            12/10/2023
-          </td>
-          <td class="white">
-            Jules
-          </td>
-          <td class="grey">
-            842 017 349
-          </td>
-          <td class="white">
-            KFC
-          </td>
-          <td class="grey">
-            05
-          </td>
-          <td class="white montant">
-            50 €
-          </td>
-        </tr>
-        <!--Mettre code PHP ici-->
-      </tbody>
-    </table>
+  <div class="mini_navbar">
+    <a class="link" href="PO_impaye_tableau.html">Tableau</a>
+    <div class="onit">Histogramme</div>
+    <a class="link" href="PO_impaye_Circu.html">Circulaire</a>
   </div>
+  <div class="canva">
+    <canvas id="mixedChart" style="width:100%;max-width:1200px;background-color: rgb(252, 248, 244);"></canvas>
+  </div>
+  <script>
+    const ctx = document.getElementById('mixedChart').getContext('2d');
+
+    const mixedChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+        datasets: [{
+            label: 'Nom client 1',
+            data: [300, 200, 400, 500, 150, 100, 50],
+            backgroundColor: 'rgba(255,7,7,0.5)',
+            font: {
+              size: 14
+            }
+          },
+          {
+            label: 'Nom client 2',
+            data: [200, 200, 200, 200, 150, 100, 50],
+            backgroundColor: 'rgba(7, 201, 255, 0.5)',
+            font: {
+              size: 14
+            }
+          },
+          {
+            label: 'Nom client 3',
+            data: [400, 250, 600, 150, 350, 100, 85], // Ce dataset est pour garder la structure
+            backgroundColor: 'rgba(181, 255, 7, 0.5)',
+            font: {
+              size: 14
+            }
+          },
+          {
+            label: 'Montant Total Dû',
+            type: 'line',
+            data: [400 + 200 + 300, 250 + 200 + 200, 600 + 200 + 400, 500 + 200 + 150, 350 + 150 + 150, 100 +
+              100 + 100, 85 + 50 + 50
+            ],
+            fill: false,
+            borderColor: 'black',
+            tension: 0.1,
+            font: 14
+          },
+        ],
+      },
+      options: {
+        responsive: false,
+        scales: {
+          x: {
+            stacked: true,
+            ticks: {
+              font: {
+                size: 25 // Change the font size for x-axis labels
+              }
+            }
+          },
+          y: {
+            stacked: true,
+            ticks: {
+              font: {
+                size: 25 // Change the font size for x-axis labels
+              }
+            }
+          },
+        },
+        plugins: {
+          tooltip: {
+            bodyFont: {
+              size: 25 // Change the font size for tooltip body
+            },
+            titleFont: {
+              size: 25 // Change the font size for tooltip title
+            },
+          },
+          legend: {
+            labels: {
+              font: {
+                size: 25 // Change the font size for legend labels
+              }
+            }
+          }
+        }
+      },
+    });
+  </script>
 </body>
 
 </html>
