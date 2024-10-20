8836012d5ce0517b6bd297e0fe51b43b7e5bb8c4
GuillaumeAugeraud <augeraud.guillaume@gmail.com>
2024-10-20 23:10:30 +0200

    Update PO_transaction.php
    Changement du tableau

--- PO/PO_transaction.php
+++ PO/PO_transaction.php
@@ -135,38 +135,115 @@ table {
         </tr>
         </thead>
         <tbody>
-        <a href="PO_transaction_detail.php">
-            <div>
-                <tr>
-                    <td class="white">
+                <tr class="view">
+                    <td>
                         0005
                     </td>
-                    <td class="grey">
+                    <td>
                         14/09/2024
                     </td>
-                    <td class="white">
+                    <td>
                         Dupont
                     </td>
-                    <td class="grey">
+                    <td>
                         425 682 301
                     </td>
-                    <td class="white">
+                    <td>
                         Vente de produit
                     </td>
-                    <td class="grey">
+                    <td>
                         985621
                     </td>
-                    <td class="white">
+                    <td>
                         E.Leclerc
                     </td>
-                    <td class="grey">
+                    <td>
                         572 183 994
                     </td>
-                    <td class="white montant">
+                    <td>
                         87152.09 €
                     </td>
                 </tr>
+             <tr class="fold">
+          <td colspan="7">
+            <div class="fold-content">
+              <h3>Company Name</h3>
+              <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Company name</th><th>Customer no</th><th>Customer name</th><th>Insurance no</th><th>Strategy</th><th>Start</th><th>Current</th><th>Diff</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Sony</td>
+                    <td>13245</td>
+                    <td>John Doe</td>
+                    <td>064578</td>
+                    <td>A, 100%</td>
+                    <td class="cur">20000</td>
+                    <td class="cur">33000</td>
+                    <td class="cur">13000</td>
+                  </tr>
+                  <tr>
+                    <td>Sony</td>
+                    <td>13288</td>
+                    <td>Claire Bennet</td>
+                    <td>064877</td>
+                    <td>B, 100%</td>
+                    <td class="cur">28000</td>
+                    <td class="cur">48000</td>
+                    <td class="cur">20000</td>
+                  </tr>
+                  <tr>
+                    <td>Sony</td>
+                    <td>12341</td>
+                    <td>Barry White</td>
+                    <td>064123</td>
+                    <td>A, 100%</td>
+                    <td class="cur">10000</td>
+                    <td class="cur">22000</td>
+                    <td class="cur">12000</td>
+                  </tr>
+                </tbody>
+              </table>          
             </div>
+          </td>
+        </tr>
+        <tr class="view">
+          <td>Company Name</td>
+          <td class="pcs">457</td>
+          <td class="cur">6535178</td>
+          <td>-</td>
+          <td class="per">50,71</td>
+          <td class="per">49,21</td>
+          <td class="per">0</td>
+        </tr>
+        <tr class="fold">
+          <td colspan="7">
+            <div class="fold-content">
+              <h3>Details</h3>
+              <table>
+               <thead>
+                  <tr>
+                    <th>Objet</th><th>Montant</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Sony</td>
+                    <td>13245</td>
+                  </tr>
+                  <tr>
+                    <td>Sony</td>
+                    <td>13288</td>
+                  </tr>
+                </tbody>
+              </table>          
+            </div>
+          </td>
+        </tr>
         <!--Mettre code PHP ici-->
         </tbody>
     </table>
