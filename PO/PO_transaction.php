a132dc3d50e012b935f6bbd3c7a183ab0ee67824
Implius <benjamin.rissot@yahoo.fr>
2024-10-22 21:55:19 +0200

    Update PO_transaction.php
    Ajout du back-end pour les détails d'une transaction

--- PO/PO_transaction.php
+++ PO/PO_transaction.php
@@ -11,98 +11,98 @@ include("../include/connexion.inc.php");
     <link rel="stylesheet" href="po.css">
     <title>JeFinance</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <script>
-    function sortTable() {
-      const select = document.getElementById('sort_by');
-      const selectedValue = select.value;
-      // Redirige vers la même page avec le paramètre de tri
-      window.location.href = `?sort_by=${selectedValue}`;
-    }
-  </script>
-  <script>
-  $(function(){
-    $(".fold-table tr.view").on("click", function(){
-      $(this).toggleClass("open").next(".fold").toggleClass("open");
-    });
-  });
-</script>
+    <script>
+        function sortTable() {
+            const select = document.getElementById('sort_by');
+            const selectedValue = select.value;
+            // Redirige vers la même page avec le paramètre de tri
+            window.location.href = `?sort_by=${selectedValue}`;
+        }
+    </script>
+    <script>
+        $(function(){
+            $(".fold-table tr.view").on("click", function(){
+                $(this).toggleClass("open").next(".fold").toggleClass("open");
+            });
+        });
+    </script>
     <style>
-    @import url('https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css');
+        @import url('https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css');
 
-* { box-sizing: border-box; }
-body { padding: .2em 2em; }
+        * { box-sizing: border-box; }
+        body { padding: .2em 2em; }
 
-table {
-  width: 100%;
-  th { text-align: left; border-bottom: 1px solid #ccc;}
-  th, td { padding: .4em; }
-}
-    table.fold-table {
-    > tbody {
-    > tr.view {
-      td, th {cursor: pointer;}
-      td:first-child, 
-      th:first-child { 
-        position: relative;
-        padding-left:20px;
-        &:before {
-          position: absolute;
-          top:50%; left:5px;
-          width: 9px; height: 16px;
-          margin-top: -8px;
-          font: 16px fontawesome;
-          color: #999;
-          content: "\f0d7";
-          transition: all .3s ease;
+        table {
+            width: 100%;
+            th { text-align: left; border-bottom: 1px solid #ccc;}
+            th, td { padding: .4em; }
         }
-      }
-      &:nth-child(2n-1) { background: #eee; }
-      &:nth-child(4n-1) { background: #cccccc; }
-      &:hover { background: #CDCDCD44; }
-      &.open {
-        background: #4e4e4e46;
-        color: white;
-        td:first-child, th:first-child {
-          &:before {
-            transform: rotate(-180deg);
-            color: #333;
-          }
+        table.fold-table {
+            > tbody {
+                > tr.view {
+                    td, th {cursor: pointer;}
+                    td:first-child,
+                    th:first-child {
+                        position: relative;
+                        padding-left:20px;
+                        &:before {
+                            position: absolute;
+                            top:50%; left:5px;
+                            width: 9px; height: 16px;
+                            margin-top: -8px;
+                            font: 16px fontawesome;
+                            color: #999;
+                            content: "\f0d7";
+                            transition: all .3s ease;
+                        }
+                    }
+                    &:nth-child(2n-1) { background: #eee; }
+                    &:nth-child(4n-1) { background: #cccccc; }
+                    &:hover { background: #CDCDCD44; }
+                    &.open {
+                        background: #4e4e4e46;
+                        color: white;
+                        td:first-child, th:first-child {
+                            &:before {
+                                transform: rotate(-180deg);
+                                color: #333;
+                            }
+                        }
+                    }
+                }
+
+                > tr.fold {
+                    display: none;
+                    &.open { display:table-row; }
+                }
+            }
         }
-      }
-    }
-  
-    > tr.fold {
-      display: none;
-      &.open { display:table-row; }
-    }
-  }
-}
 
 
 
-.fold-content {
-  padding: .5em;
-  h3 { margin-top:0; }
-  > table {
-    border: 2px solid #ccc;
-    > tbody {
-      tr:nth-child(even) {
-        background: #eee;
-      }
-      tr:nth-child(odd) {
-        background: #b3b3b3;
-      }
-    }
-  }
-}
-  </style>
+        .fold-content {
+            padding: .5em;
+            h3 { margin-top:0; }
+            > table {
+                border: 2px solid #ccc;
+                > tbody {
+                    tr:nth-child(even) {
+                        background: #eee;
+                    }
+                    tr:nth-child(odd) {
+                        background: #b3b3b3;
+                    }
+                }
+            }
+        }
+    </style>
 </head>
 
 <body>
 
 <?php
-    $onit = "Remise";
-    include("../include/po_navbar_w_return.inc.php"); // Navbar
+$onit = "Remise";
+include("../include/po_navbar_w_return.inc.php"); // Navbar
 ?>
 
 <div class="Compte_tableau">
@@ -116,7 +116,7 @@ table {
                 Date
             </th>
             <th class="table-darkblue">
-                Num Carte 
+                Num Carte
             </th>
             <th class="table-blue">
                 Objet
@@ -147,92 +147,94 @@ table {
 
         ?>
         <tbody>
-                <tr class="view">
-                    
-                    <td>
-                        <?php echo $donnees->id_remise; ?>
-                    </td>
-                    <td>
-                        <?php echo $donnees->date_trans; ?>
-                    </td>
-                    <td>
-                        <?php $carte = $donnees->num_carte;
-                        $ano = "**** **** **** ".substr($carte,-4,4);
-                        echo $ano;?>
-                    </td>
-                    <td>
-                        <?php echo $donnees->libelle; ?>
-                    </td>
-                    <td>
-                        <?php echo $donnees->num_autorisation; ?>
-                    </td>
-                    <td>
-                        <?php echo $donnees->raison_autre_parti; ?>
-                    </td>
-                    <td>
-                        <?php echo $donnees->siren_autre_parti; ?>
-                    </td>
-                    <td>
-                        <?php $join_query = $cnx->query("select compte.devise from compte join remise on compte.num_siren = remise.num_siren;");
-                        $devise = $join_query->fetch(PDO::FETCH_OBJ)->devise;
-                        switch ($devise) {
-                            case "EUR":
-                                $devise = " €";
-                                break;
-                            case "USD":
-                                $devise = " $";
-                                break;
-                            case "GBP":
-                                $devise = " £";
-                                break;
-                            default:
-                                $devise = " ?";
-                                break;
-                        }
-                        $remise = $ligne->id_remise;
-                        $sql = "SELECT * FROM bank.transaction WHERE id_remise='".$remise."';";
-                        $req2 = $cnx->query($sql);
-                        $montant = 0;
-                        while ($donnees = $req2->fetch(PDO::FETCH_OBJ)) {
-                            $montant += $donnees->montant;
-                        }
-                        if ($ligne->sens == '-') {
-                            echo "<p class=\"red\">";
-                            echo "- ".$montant; // montant
-                            echo "</p>";
-                        }
-                        else {
-                            echo $montant.$devise; // montant
-                        }
-                        }}?>
-                    </td>
-                </tr>
-             <tr class="fold">
-                 
-          <td colspan="7">
-            <div class="fold-content">
-              <h3>Details</h3>
-              <table>
-               <thead>
-                  <tr>
-                    <th>Objet</th><th>Montant</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>Sony</td>
-                    <td>13245</td>
-                  </tr>
-                  <tr>
-                    <td>Sony</td>
-                    <td>13288</td>
-                  </tr>
-                </tbody>
-              </table>          
-            </div>
-          </td>
+        <tr class="view">
+
+            <td>
+                <?php echo $donnees->id_remise; ?>
+            </td>
+            <td>
+                <?php echo $donnees->date_trans; ?>
+            </td>
+            <td>
+                <?php $carte = $donnees->num_carte;
+                $ano = "**** **** **** ".substr($carte,-4,4);
+                echo $ano;?>
+            </td>
+            <td>
+                <?php echo $donnees->libelle; ?>
+            </td>
+            <td>
+                <?php echo $donnees->num_autorisation; ?>
+            </td>
+            <td>
+                <?php echo $donnees->raison_autre_parti; ?>
+            </td>
+            <td>
+                <?php echo $donnees->siren_autre_parti; ?>
+            </td>
+            <td>
+                <?php $join_query = $cnx->query("select compte.devise from compte join remise on compte.num_siren = remise.num_siren;");
+                $devise = $join_query->fetch(PDO::FETCH_OBJ)->devise;
+                switch ($devise) {
+                    case "EUR":
+                        $devise = " €";
+                        break;
+                    case "USD":
+                        $devise = " $";
+                        break;
+                    case "GBP":
+                        $devise = " £";
+                        break;
+                    default:
+                        $devise = " ?";
+                        break;
+                }
+                $trans = $donnees->id_trans;
+                $sql = "SELECT * FROM bank.detail WHERE id_trans='".$trans."';";
+                $reqD = $cnx->query($sql);
+                $montant = 0;
+                while ($ligne = $reqD->fetch(PDO::FETCH_OBJ)) {
+                    $montant += $ligne->montant;
+                }
+                if ($donnees->sens == '-') {
+                    echo "<p class=\"red\">";
+                    echo "- ".$montant.$devise; // montant
+                    echo "</p>";
+                }
+                else {
+                    echo $montant->montant.$devise; // montant
+                }?>
+            </td>
         </tr>
-        <!--Mettre code PHP ici-->
+        <tr class="fold">
+            <?php //requete pour les détails de la transactions cliqué
+            $sql = "SELECT * FROM bank.detail WHERE id_trans = '".$donnees->id_trans."';";
+            $reqT = $cnx->query($sql);
+            ?>
+            <td colspan="7">
+                <div class="fold-content">
+                    <h3>Details</h3>
+                    <table>
+                        <thead>
+                        <tr>
+                            <th>Objet</th><th>Montant</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php while ($donneesT = $reqT->fetch(PDO::FETCH_OBJ)) { ?>
+                        <tr>
+                            <td><?php echo $donneesT->libelle; ?></td>
+                            <td><?php echo "<p class=\"red\">";
+                                if ($donneesT->sens == '+') echo $donneesT->montant.$devise;
+                                if ($donneesT->sens == '-') echo '- '.$donneesT->montant.$devise;?></td>
+                        </tr>
+                        <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
+            </td>
+        </tr>
+        <?php } ?>
         </tbody>
     </table>
 </div>
