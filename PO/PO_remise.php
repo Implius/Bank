ef282508f194d4a717e42bc8af206198f865813a
Implius <benjamin.rissot@yahoo.fr>
2024-10-22 17:21:03 +0200

    Update PO_remise.php

--- PO/PO_remise.php
+++ PO/PO_remise.php
@@ -144,15 +144,21 @@ include("../include/po_navbar.inc.php"); // Navbar
                             $devise = " ?";
                             break;
                     }
-
-                    if ($ligne->sens == '-') {
-                        echo "<p class=\"red\">";
-                        echo "- ".$ligne->montant.$devise; // montant
-                        echo "</p>";
-                    }
-                    else {
-                        echo $ligne->montant.$devise; // montant
-                    }
+                        $remise = $ligne->id_remise;
+                        $sql = "SELECT * FROM bank.transaction WHERE id_remise='".$remise."';";
+                        $req2 = $cnx->query($sql);
+                        $montant = 0;
+                        while ($donnees = $req2->fetch(PDO::FETCH_OBJ)) {
+                            $montant += $donnees->montant;
+                        }
+                        if ($ligne->sens == '-') {
+                            echo "<p class=\"red\">";
+                            echo "- ".$montant; // montant
+                            echo "</p>";
+                        }
+                        else {
+                            echo $montant.$devise; // montant
+                        }
                     ?>
                 </td>
             </tr>
