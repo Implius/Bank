e7c12b619ceb25e83cf0879ee37e9a0e060b7b2c
Implius <benjamin.rissot@yahoo.fr>
2024-10-22 15:15:51 +0200

    Update PO_remise.php
    Ajout des pages transactions en lien avec la ligne sur laquelle on clique

--- PO/PO_remise.php
+++ PO/PO_remise.php
@@ -19,6 +19,16 @@ include('../include/verifyconnexion.inc.php');
             window.location.href = `?sorta_by=${selectedValue}`;
         }
     </script>
+    <script>
+        function redirectTo(url) {
+            window.location.href = url;
+        }
+    </script>
+    <style>
+        tr{
+            cursor: pointer;
+        }
+    </style>
 </head>
 
 <body>
@@ -88,7 +98,7 @@ include("../include/po_navbar.inc.php"); // Navbar
         $req = $cnx->query("SELECT * FROM remise".$tri);
         while ($ligne = $req->fetch(PDO::FETCH_OBJ)) {
             echo "<a href=\"PO_transaction.php?id_remise=".$ligne->id_remise.">"; ?>
-            <tr>
+            <tr onclick="redirectTo('PO_transaction.php?id_remise=<?php echo $ligne->id_remise; ?>')">
                 <td>
                     <?php echo $ligne->id_remise; // id remise ?>
                 </td>
