e219ce41687f4425b642ad24f614184c92983d5a
GuillaumeAugeraud <augeraud.guillaume@gmail.com>
2024-10-22 11:10:58 +0200

    Update po_navbar.inc.php
    Changements des liens

--- include/po_navbar.inc.php
+++ include/po_navbar.inc.php
@@ -9,7 +9,7 @@ switch ($onit) {
         ?>
         <div class="onit">Compte</div>
         <a class="link" href="../PO/PO_remise.php">Remise</a>
-        <a class="link" href="../PO/PO_impaye_Circu.php">Impayé</a>
+        <a class="link" href="../PO/PO_impaye_tableau.php">Impayé</a>
         <a class="link" href="../PO/PO_creation.php">Création</a>
         <?php
         break;
@@ -18,7 +18,7 @@ switch ($onit) {
         ?>
         <a class="link" href="../PO/po_compte.php">Compte</a>
         <div class="onit">Remise</div>
-        <a class="link" href="../PO/PO_impaye_Circu.php">Impayé</a>
+        <a class="link" href="../PO/PO_impaye_tableau.php">Impayé</a>
         <a class="link" href="../PO/PO_creation.php">Création</a>
         <?php
         break;
@@ -36,7 +36,7 @@ switch ($onit) {
         ?>
         <a class="link" href="../PO/po_compte.php">Compte</a>
         <a class="link" href="../PO/PO_remise.php">Remise</a>
-        <a class="link" href="../PO/PO_impaye_Circu.php">Impayé</a>
+        <a class="link" href="../PO/PO_impaye_tableau.php">Impayé</a>
         <div class="onit">Création</div>
         <?php
         break;
