efdce400c67708077a6fb90c6e7a99e961b959a0
Andrei Achirecesei <90113072+mickeyilestmignon@users.noreply.github.com>
2024-10-24 16:33:25 +0200

    ajout du tri pour les comptes + nombre de lignes

--- PO/po_compte.php
+++ PO/po_compte.php
@@ -13,11 +13,35 @@ include('../include/verifyconnexion.inc.php');
   <link rel="stylesheet" href="PO.css">
   <title>JeFinance</title>
     <script>
+        var dict = {
+            "sort_by": "",
+            "search": ""
+        };
+
+        const urlParams = new URLSearchParams(window.location.search);
+
         function sortTable() {
-            const select = document.getElementById('sort_by');
-            const selectedValue = document.getElementById('sort_by');
-            // Redirige vers la même page avec le paramètre de tri
-            window.location.href = `?sort_by=${selectedValue.value}`;
+            var selectedValue = document.getElementById('sort_by');
+            var url = String(window.location.href).split('?')[0];
+            url = url.concat("?sort_by=".concat(selectedValue.value));
+            var search = urlParams.get('search');
+            if (search == null) {
+                search = "";
+            }
+            url = url.concat("&search=".concat(search));
+            window.location.href = url;
+        }
+
+        function search() {
+            var selectedValue = document.getElementById('search');
+            var url = String(window.location.href).split('?')[0];
+            url = url.concat("?search=".concat(selectedValue.value));
+            var sort_by = urlParams.get('sort_by');
+            if (sort_by == null) {
+                sort_by = "";
+            }
+            url = url.concat("&sort_by=".concat(sort_by));
+            window.location.href = url;
         }
   </script>
 </head>
@@ -30,6 +54,12 @@ include("../include/po_navbar.inc.php"); // Navbar
 ?>
 
   <div class="Compte_tableau">
+      <div class="search">
+            <form onchange="search()">
+                <input type="text" name="search" id="search" <?php if(isset($_GET["search"])) { echo "placeholder=\"".$_GET["search"]."\""; } else { echo "placeholder=\"Rechercher un compte\""; } ?>>
+                <button type="submit">Rechercher</button>
+            </form>
+      </div>
     <div class="sorting">
       Trier par :
       <select name="sort_by" id="sort_by" onchange="sortTable()">
@@ -54,6 +84,26 @@ include("../include/po_navbar.inc.php"); // Navbar
       </select>
     </div>
 
+      <?php
+      if (isset($_GET["sort_by"])) {
+          $tri = match ($_GET["sort_by"]) {
+              "tresorerie" => "ORDER BY tresorerie DESC",
+              "nb_remises" => "ORDER BY (select count(*) from remise where remise.num_siren = compte.num_siren) DESC",
+              "nb_impayes" => "ORDER BY (select count(*) from impaye where impaye.num_siren = compte.num_siren) DESC",
+              "num_siren" => "ORDER BY num_siren",
+          };
+      } else {
+          $tri = "";
+      }
+      if (isset($_GET["search"])) {
+          $search = "where raison_social like '%" . $_GET["search"] . "%' ";
+      } else {
+          $search = "";
+      }
+      $req = $cnx->query("SELECT * FROM compte ".$search.$tri);
+      echo "Nombre de lignes : ".$req->rowCount();
+      ?>
+
     <table class="tableau">
       <thead>
         <tr>
@@ -73,17 +123,6 @@ include("../include/po_navbar.inc.php"); // Navbar
       </thead>
       <tbody>
       <?php
-      if (isset($_GET["sort_by"])) {
-          $tri = match ($_GET["sort_by"]) {
-                "tresorerie" => "ORDER BY tresorerie DESC",
-                "nb_remises" => "ORDER BY (select count(*) from remise where remise.num_siren = compte.num_siren) DESC",
-                "nb_impayes" => "ORDER BY (select count(*) from impaye where impaye.num_siren = compte.num_siren) DESC",
-                "num_siren" => "ORDER BY num_siren",
-          };
-      } else {
-          $tri = "";
-      }
-        $req = $cnx->query("SELECT * FROM compte ".$tri);
         while ($ligne = $req->fetch(PDO::FETCH_OBJ)) {
             ?>
             <tr>
