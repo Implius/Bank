dfe99d92b266543d8a4aa7f9a288170a6631e23b
Implius <benjamin.rissot@yahoo.fr>
2024-11-08 12:09:33 +0100

    ADD btn XLS export

--- PO/PO_impaye_tableau.php
+++ PO/PO_impaye_tableau.php
@@ -18,13 +18,15 @@ include("../include/connexion.inc.php");
             window.location.href = `?sort_by=${selectedValue}`;
         }
     </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.8.4/jspdf.plugin.autotable.min.js"></script>
     <style>
         .button_tel{
             margin-top: 30px;
             display: flex;
             justify-content: center;
         }
-        #btn{
+        .button_tel button{
             padding: 25px 25px;
             border-radius: 15px;
             background-color: #c1c1c1;
@@ -90,7 +92,7 @@ include("../include/po_navbar.inc.php"); // Navbar
     echo "Nombre de lignes : ".$req->rowCount();
     ?>
 
-    <table class="tableau">
+    <table class="tableau" id="table_imp">
         <thead>
         <tr>
             <th class="table-blue">
@@ -162,9 +164,45 @@ include("../include/po_navbar.inc.php"); // Navbar
 
 </div>
 <div class="button_tel">
-    <button id="btn">Exporter format CSV</button>
+    <button id="btn_csv">Exporter format CSV</button>
+    <button id="btn_pdf">Exporter format PDF</button>
+    <button id="btn_xls">Exporter format XLS</button>
 </div>
 <script src="../script.js">
+</script>
+    
+<script>
+    function exportTableToExcel(tableId) {
+
+        // Get the table element using the provided ID
+        const table = document.getElementById(tableId);
+
+        // Extract the HTML content of the table
+        const html = table.outerHTML;
+
+        // Create a Blob containing the HTML data with Excel MIME type
+        const blob = new Blob([html], {type: 'application/vnd.ms-excel'});
+
+        // Create a URL for the Blob
+        const url = URL.createObjectURL(blob);
+
+        // Create a temporary anchor element for downloading
+        const a = document.createElement('a');
+        a.href = url;
+
+        // Set the desired filename for the downloaded file
+        a.download = 'table.xls';
+
+        // Simulate a click on the anchor to trigger download
+        a.click();
+
+        // Release the URL object to free up resources
+        URL.revokeObjectURL(url);
+    }
+
+    document.getElementById('btn_xls').addEventListener('click', function() {
+        exportTableToExcel('table_imp');
+    });
 </script>
 </body>
 
