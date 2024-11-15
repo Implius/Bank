dc08f80fbcf030c867796496d08ea469cd265937
Implius <benjamin.rissot@yahoo.fr>
2024-11-15 15:10:34 +0100

    Update User_tresorerie.php

--- User/User_tresorerie.php
+++ User/User_tresorerie.php
@@ -2,115 +2,245 @@
 global $cnx;
 include("../include/connexion.inc.php");
 include('../include/verifyconnexion_user.inc.php');
-if (isset($_GET['num_siren'])) {
-    $_SESSION["num_siren"] = $_GET["num_siren"];
+if (isset($_SESSION['NumSiren'])) {
+    $numsiren = $_SESSION["NumSiren"];
 }
 ?>
-    <!doctype html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-        <link rel="stylesheet" href="po_compte.css">
-        <title>JeFinance</title>
-    </head>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <link rel="stylesheet" href="po_compte.css">
+    <title>JeFinance</title>
+</head>
 <body>
 <?php
 $onit = "Tresorerie";
 include("../include/User_navbar.inc.php"); // Navbar
 ?>
-    <div class="Compte_title_sup">
-        <div class="Compte_title">
-            <h1 class="tresorerie">Nom de l'entreprise</h1>
-            <h2 class="subtitle1">Num SIREN: <?php echo $_SESSION["num_siren"]?></h2>
-            <h2 class="subtitle2">Num Compte: 2222</h2>
-            <h2 class="green">Total argent</h2>
+<div class="Compte_title_sup">
+    <div class="Compte_title">
+        <h1 class="tresorerie"><?php
+            $stmt = $cnx->prepare("SELECT raison_social FROM compte WHERE num_siren = :num_siren");
+            $stmt->bindParam(':num_siren', $_SESSION["NumSiren"]);
+            $stmt->execute();
+            $name = $stmt->fetch(PDO::FETCH_OBJ)->raison_social;
+            echo $name;
+            ?></h1>
+        <h2 class="subtitle1">Num SIREN: <?php echo $_SESSION["NumSiren"]?></h2>
+        <h2 class="subtitle2">Num Compte: 2222</h2>
+        <h2 class="green"><?php
+            $tresorerie = $cnx->prepare("SELECT tresorerie from compte where num_siren=:num_siren;");
+            $tresorerie->bindParam(':num_siren', $_SESSION["NumSiren"]);
+            $tresorerie->execute();
+            echo $tresorerie->fetchColumn();
+            ?></h2>
     </div>
-    <div class="canva">
-        <canvas id="mixedChart" style="width:100%;max-width:1200px;background-color: rgb(252, 248, 244);"></canvas>
-    </div>
-    <script>
-        const ctx = document.getElementById('mixedChart').getContext('2d');
-        const mixedChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
-                datasets: [{
-                    label: 'Nom client 1',
-                    data: [300, 200, 400, 500, 150, 100, 50],
-                    backgroundColor: 'rgba(255,7,7,0.5)',
-                    font: {
-                        size: 14
+</div>
+<div class="canva">
+    <canvas id="mixedChart" style="width:100%;max-width:1200px;background-color: rgb(252, 248, 244);"></canvas>
+</div>
+
+<div class="sorting">
+    Trier par :
+    <select name="month_by" id="month_by" onChange="sortTable()">
+        <option value="" disabled selected><?php
+            if (isset($_GET["month_by"])) {
+                $tri = $_GET["month_by"];
+                echo match ($tri) {
+                    "4month" => "4 months",
+                    "6month" => "6 months",
+                    "12month" => "12 months",
+                    default => "$tri months",
+                };
+            } else {
+                echo "6 months";
+                $tri = "6";
+            }
+
+            ?></option>
+        <option value="4">4 months</option>
+        <option value="6">6 months</option>
+        <option value="12">12 months</option>
+    </select>
+</div>
+<?php
+function addMonth($year,$month,$nb){
+    //Ajoute $nb aux mois et 1 à l'année si elle est passé en ajoutant le mois
+    for ($i=1; $i <= $nb; $i++) {
+        if (($month + 1) > 12) {
+            $month = 1;
+            $year++;
+        } else {
+            $month += 1;
+        }
+    }
+    return [$month,$year]; //return un tableau car impossible de renvoyer plusieurs valeurs
+}
+
+function getMonthMin($year,$month,$i){
+    //Permet d'avoir le premier mois de la borne
+    //$i permet de savoir l'intervalle entre les 2 (6 mois, 12 mois, etc...)
+    for ($j = 0; $j < $i; $j++){
+        if (($month - 1) <= 0) {
+            $month = 12;
+            $year--;
+        } else {
+            $month -= 1;
+        }
+    }
+    return [$month,$year];
+}
+
+function monthToString($month){
+    //Return le mois en string au lieu de son numero
+    $months = ["Jan","Feb","March","Avr","May","June","July","Aout","Sep","Oct","Nov","Dec"];
+    return $months[$month-1];
+}
+
+//permet d'avoir le derniers mois enregistrer dans la bdd
+$sql = "Select max(bank.remise.date_remise) FROM bank.remise;";
+$datemax = $cnx->query($sql)->fetch();
+
+//La partie qui permet d'avoir les données de départ pour commencer
+//L'initialisation en gros
+$yearmax = substr($datemax[0],0,4);
+$yearmin = $yearmax;
+$monthend = substr($datemax[0],5,2);
+$monthmin = $monthend;
+$inter = (int)$tri;
+$tmp = getMonthMin($yearmax,$monthend,$inter-1);
+$monthmin = $tmp[0];
+$yearmin = $tmp[1];
+$year = $yearmin;
+$day = 1;
+$hour = 0;
+$min = 0;
+$sec = 0;
+
+//Début du traitement des infos
+
+//Initialisation d'un tableau qui contiendra les num siren et leur valeurs associé le tout associer à un mois (1 les premiers, 2 le deuxième, etc...)
+$annee = [];
+for ($i = 1; $i <= $inter; $i++) {
+    $annee[$i] = [];
+}
+
+//On initialise un tableau qui gardera en mémoire les différents siren sur les derniers mois
+$num_siren = array();
+
+//traitement
+for ($m = 1; $m != $inter+1; $m++){
+    //permet la borne entre le premiers mois et le suivant pour la requête sql
+    $tmp2 = addMonth($year,$monthmin,1);
+
+    $month = $tmp2[0];
+    $year = $tmp2[1];
+
+    //requête sql (utilisant make_timestamp pour pourvoir faire un between en utilisant des type timestamp)
+    $numsiren = $_SESSION['NumSiren'];
+    $sql = "SELECT * FROM bank.remise WHERE date_remise BETWEEN make_date($yearmin,$monthmin,$day) AND make_date($year,$month,$day) AND num_siren='$numsiren' ORDER BY date_remise;";
+    $req = $cnx->query($sql);
+    $montant = 0;
+    $annee[$m][$numsiren] = 0;
+    while ($row = $req->fetch(PDO::FETCH_OBJ)) {
+        $siren = $row->num_siren;
+        $sens = $row->sens;
+        if ($sens=="+") {
+            $annee[$m][$siren] += $row->montant; //Ajoute le montant
+        } else {
+            $annee[$m][$siren] -= $row->montant;
+        }
+        //Prend le siren associer à ce montant
+        if (!in_array($siren,$num_siren)) {array_push($num_siren,$siren);} //Ajoute le siren dans liste des sirens (si il y est pas déjà)
+    }
+    // On augmente la borne de 1 mois
+    $monthmin = $month;
+    $yearmin = $year;
+}
+?>
+<script>
+    const ctx = document.getElementById('mixedChart').getContext('2d');
+
+    const mixedChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: [<?php for ($m = 1; $m != $inter+1; $m++){
+                if ($m != $inter){
+                    echo '"'.monthToString(addmonth(getMonthMin($year,$monthend,$inter)[1],getMonthMin($year,$monthend,$inter)[0],$m)[0])." ".addmonth(getMonthMin($year,$monthend,$inter)[1],getMonthMin($year,$monthend,$inter)[0],$m)[1].'",';
+                } else {
+                    echo '"'.monthToString(addmonth(getMonthMin($year,$monthend,$inter)[1],getMonthMin($year,$monthend,$inter)[0],$m)[0])." ".addmonth(getMonthMin($year,$monthend,$inter)[1],getMonthMin($year,$monthend,$inter)[0],$m)[1].'"';    }
+            }?>],
+            datasets: [
+                //On va faire les données dans l'histogramme
+                {
+                <?php
+                echo "label: 'Montant Total',\n";
+                echo "type: 'line',\n";
+                echo "data: [";
+                for ($m = 1; $m != $inter+1; $m++){
+                    $data = 0;
+                    if (!empty($annee[$m])){
+                        foreach($annee[$m] as $montant){
+                            $data += $montant;
+                        }
+                    }
+                    if ($m == $inter){
+                        echo $data;
+                    } else {
+                        echo $data.", ";
+                    }
+                }
+                echo "],\n";
+                echo "fill: false,\n";
+                echo "borderColor: 'black',\n";
+                echo "tension: 0.1,\n";
+                echo "font: 14},";
+                ?>
+            ],
+        },
+        options: {
+            responsive: false,
+            scales: {
+                x: {
+                    stacked: true,
+                    ticks: {
+                        font: {
+                            size: 25 // Change the font size for x-axis labels
+                        }
                     }
                 },
-                    {
-                        label: 'Nom client 2',
-                        data: [200, 200, 200, 200, 150, 100, 50],
-                        backgroundColor: 'rgba(7, 201, 255, 0.5)',
+                y: {
+                    stacked: true,
+                    ticks: {
                         font: {
-                            size: 14
+                            size: 25 // Change the font size for x-axis labels
                         }
-                    },
-                    {
-                        label: 'Nom client 3',
-                        data: [400, 250, 600, 150, 350, 100, 85], // Ce dataset est pour garder la structure
-                        backgroundColor: 'rgba(181, 255, 7, 0.5)',
-                        font: {
-                            size: 14
-                        }
-                    },
-                    {
-                        label: 'Montant Total Dû',
-                        type: 'line',
-                        data: [400 + 200 + 300, 250 + 200 + 200, 600 + 200 + 400, 500 + 200 + 150, 350 + 150 + 150, 100 +
-                        100 + 100, 85 + 50 + 50
-                        ],
-                        fill: false,
-                        borderColor: 'black',
-                        tension: 0.1,
-                        font: 14
-                    },
-                ],
+                    }
+                },
             },
-            options: {
-                responsive: false,
-                scales: {
-                    x: {
-                        stacked: true,
-                        ticks: {
-                            font: {
-                                size: 25 // Change the font size for x-axis labels
-                            }
-                        }
+            plugins: {
+                tooltip: {
+                    bodyFont: {
+                        size: 25 // Change the font size for tooltip body
                     },
-                    y: {
-                        stacked: true,
-                        ticks: {
-                            font: {
-                                size: 25 // Change the font size for x-axis labels
-                            }
-                        }
+                    titleFont: {
+                        size: 25 // Change the font size for tooltip title
                     },
                 },
-                plugins: {
-                    tooltip: {
-                        bodyFont: {
-                            size: 25 // Change the font size for tooltip body
-                        },
-                        titleFont: {
-                            size: 25 // Change the font size for tooltip title
-                        },
-                    },
-                    legend: {
-                        labels: {
-                            font: {
-                                size: 25 // Change the font size for legend labels
-                            }
+                legend: {
+                    labels: {
+                        font: {
+                            size: 25 // Change the font size for legend labels
                         }
                     }
                 }
-            },
-        });
-    </script>
+            }
+        },
+    });
+</script>
 </body>
+
+</html>
