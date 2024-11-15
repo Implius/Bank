265b79beee96991553b6482b3e8bb9224b20a0b6
Andrei Achirecesei <90113072+mickeyilestmignon@users.noreply.github.com>
2024-11-15 16:26:05 +0100

    recherche + nb de lignes

--- PO/po_compte.php
+++ PO/po_compte.php
@@ -17,10 +17,18 @@ if (isset($_SESSION['num_siren'])) {
   <title>JeFinance</title>
     <script>
         function sortTable() {
-            const select = document.getElementById('sort_by');
             const selectedValue = document.getElementById('sort_by');
-            // Redirige vers la même page avec le paramètre de tri
-            window.location.href = `?sort_by=${selectedValue.value}`;
+            // Si l'url contient un paramètre search
+            if (window.location.href.includes("?")) {
+                // On redirige vers la même page avec le paramètre sort_by
+                if (window.location.href.includes("&sort_by=")) {
+                    window.location.href = window.location.href.split("&sort_by=")[0] + "&sort_by=" + selectedValue.value;
+                } else {
+                    window.location.href = `?search=${window.location.href.split("?search=")[1]}&sort_by=${selectedValue.value}`;
+                }
+            } else {
+                window.location.href = `?sort_by=${selectedValue.value}`;
+            }
         }
   </script>
 </head>
@@ -30,9 +38,22 @@ if (isset($_SESSION['num_siren'])) {
 <?php
 $onit = "Compte";
 include("../include/po_navbar.inc.php"); // Navbar
+if (!isset($_GET['search']) || $_GET['search'] == "") {
+    $search = "";
+} else {
+    $search = "WHERE raison_social LIKE '%".$_GET['search']."%' ";
+}
 ?>
 
   <div class="Compte_tableau">
+
+      <div class="sorting">
+            <form action="po_compte.php" method="get">
+                <input type="text" name="search" placeholder="<?php if ($search == "") { echo "Rechercher"; } else { echo $_GET['search']; } ?>">
+                <button type="submit"><?php if ($search == "") { echo "Rechercher"; } else { echo "Supprimer"; } ?></button>
+            </form>
+      </div>
+
     <div class="sorting">
       Trier par :
       <select name="sort_by" id="sort_by" onchange="sortTable()">
@@ -57,6 +78,21 @@ include("../include/po_navbar.inc.php"); // Navbar
       </select>
     </div>
 
+      <?php
+      if (isset($_GET["sort_by"])) {
+          $tri = match ($_GET["sort_by"]) {
+                "tresorerie" => "ORDER BY tresorerie DESC",
+                "nb_remises" => "ORDER BY (select count(*) from remise where remise.num_siren = compte.num_siren) DESC",
+                "nb_impayes" => "ORDER BY (select count(*) from impaye where impaye.num_siren = compte.num_siren) DESC",
+                "num_siren" => "ORDER BY num_siren",
+          };
+      } else {
+          $tri = "";
+      }
+        $req = $cnx->query("SELECT * FROM compte ".$search.$tri);
+      echo "<p class='nb_lignes'>Nombre de comptes : ".$req->rowCount()."</p>";
+      ?>
+
     <table class="tableau">
       <thead>
         <tr>
@@ -76,17 +112,6 @@ include("../include/po_navbar.inc.php"); // Navbar
       </thead>
       <tbody>
       <?php
-      if (isset($_GET["sort_by"])) {
-          $tri = match ($_GET["sort_by"]) {
-                "tresorerie" => "ORDER BY tresorerie DESC",
-                "nb_remises" => "ORDER BY (select count(*) from remise where remise.num_siren = compte.num_siren) DESC",
-                "nb_impayes" => "ORDER BY (select count(*) from impaye where impaye.num_siren = compte.num_siren) DESC",
-                "num_siren" => "ORDER BY num_siren",
-          };
-      } else {
-          $tri = "";
-      }
-        $req = $cnx->query("SELECT * FROM compte ".$tri);
         while ($ligne = $req->fetch(PDO::FETCH_OBJ)) {
             ?>
             <tr onclick="document.location = 'PO_Compte_Tresorerie.php?num_siren=<?php echo $ligne->num_siren; ?>';">
