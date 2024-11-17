e876fe87c51e7631c4beb1a60685cb8f105e0082
Implius <benjamin.rissot@yahoo.fr>
2024-11-17 16:08:29 +0100

    Update PO_impaye_Histo.php

--- PO/PO_impaye_Histo.php
+++ PO/PO_impaye_Histo.php
@@ -30,6 +30,18 @@ include("../include/connexion.inc.php");
 <?php
 $onit = "Impaye";
 include("../include/po_navbar.inc.php"); // Navbar
+if (isset($_POST["date_begin"])) {
+    $date_begin = $_POST["date_begin"];
+} else {
+    $date_begin = null;
+}
+if (isset($_POST["date_end"])) {
+    $date_end = $_POST["date_end"];
+} else {
+    $date_end = null;
+}
+print_r($date_end);
+print_r($date_begin);
 ?>
 
   <div class="mini_navbar">
@@ -42,34 +54,48 @@ include("../include/po_navbar.inc.php"); // Navbar
   </div>
 
 <div class="sorting">
+    <div class="month">
     Trier par :
     <select name="month_by" id="month_by" onChange="sortTable()" class="tri_histo">
-        <option value="" disabled selected><?php
-            if (isset($_GET["month_by"])) {
-                $tri = $_GET["month_by"];
-                echo match ($tri) {
-                    "4month" => "4 months",
-                    "6month" => "6 months",
-                    "12month" => "12 months",
-                    default => "$tri months",
-                };
-            } else {
-                echo "6 months";
-                $tri = "6";
-            }
+            <option value="" disabled selected><?php
+                if (isset($_GET["month_by"])) {
+                    $tri = $_GET["month_by"];
+                    echo match ($tri) {
+                        "4month" => "4 months",
+                        "6month" => "6 months",
+                        "12month" => "12 months",
+                        default => "$tri months",
+                    };
+                } else {
+                    echo "6 months";
+                    $tri = "6";
+                }
+                if (isset($_POST['reset'])){
+                    $date_begin = null;
+                    $date_end = null;
+                }
 
-            ?></option>
-        <option value="4">4 months</option>
-        <option value="6">6 months</option>
-        <option value="12">12 months</option>
-    </select>
+                ?></option>
+            <option value="4">4 months</option>
+            <option value="6">6 months</option>
+            <option value="12">12 months</option>
+        </select>
+    </div>
+    <div class="date">
+        <form action="" method="POST">
+            <div><label> To : </label> <input name="date_begin" type="date" min="2020-01-01" <?php if($date_begin != null) echo "value='".$date_begin."'" ?>/></div>
+            <div><label> From : </label> <input name="date_end" type="date" min="2020-01-01" <?php if($date_end != null) echo "value='".$date_end."'" ?>/></div>
+            <div><button value="submit"> Submit </button> <button value="reset" name="reset"> Reset </button></div>
+        </form>
+    </div>
 </div>
 
 <?php
     global $cnx;
     include("../include/connexion.inc.php");
 
-    function addMonth($year,$month,$nb){
+    function addMonth($year,$month,$nb): array
+    {
         //Ajoute $nb aux mois et 1 à l'année si elle est passé en ajoutant le mois
         for ($i=1; $i <= $nb; $i++) {
             if (($month + 1) > 12) {
@@ -82,7 +108,8 @@ include("../include/po_navbar.inc.php"); // Navbar
         return [$month,$year]; //return un tableau car impossible de renvoyer plusieurs valeurs
     }
 
-    function getMonthMin($year,$month,$i){
+    function getMonthMin($year,$month,$i): array
+    {
         //Permet d'avoir le premier mois de la borne
         //$i permet de savoir l'intervalle entre les 2 (6 mois, 12 mois, etc...)
         for ($j = 0; $j < $i; $j++){
@@ -96,67 +123,109 @@ include("../include/po_navbar.inc.php"); // Navbar
         return [$month,$year];
     }
 
-    function monthToString($month){
+    function monthToString($month): string
+    {
         //Return le mois en string au lieu de son numero
         $months = ["Jan","Feb","March","Avr","May","June","July","Aout","Sep","Oct","Nov","Dec"];
         return $months[$month-1];
     }
 
+    function compareMonth($monthA,$monthB) : int
+    {
+        $monthtmp = $monthB - $monthA;
+        if ($monthtmp == 0){
+            return 1;
+        } else {
+            print_r($monthtmp);
+            return $monthtmp + 1;
+        }
+    }
+    if ($date_begin != null && $date_end != null) {
+        $date_begin = (string)$date_begin;
+        $year_begin = substr($date_begin, 0, 4);
+        $month_begin = substr($date_begin, 5, 2);
+        $day_begin = substr($date_begin, 8, 2);
+        $date_end = (string)$date_end;
+        $year_end = substr($date_end, 0, 4);
+        $month_end = substr($date_end, 5, 2);
+        $day_end = substr($date_end, 8, 2);
+    }
     //permet d'avoir le derniers mois enregistrer dans la bdd
-    $sql = "Select max(impaye.date_impaye) FROM bank.impaye;";
+    if ($date_begin == null){
+        $sql = "Select max(impaye.date_impaye) FROM bank.impaye;";
+    } else if ($date_end == null){
+        $sql = "Select max(impaye.date_impaye) FROM bank.impaye WHERE date_impaye = make_timestamp($year_begin,$month_begin,$day_begin,0,0,0);";
+    } else {
+        $sql = "Select max(date_impaye) FROM bank.impaye WHERE date_impaye BETWEEN make_timestamp($year_begin,$month_begin,$day_begin,0,0,0) and make_timestamp($year_end,$month_end,$day_end,0,0,0);";
+    }
+
     $datemax = $cnx->query($sql)->fetch();
 
     //La partie qui permet d'avoir les données de départ pour commencer
     //L'initialisation en gros
-    $yearmax = substr($datemax[0],0,4);
-    $yearmin = $yearmax;
-    $monthend = substr($datemax[0],5,2);
-    $monthmin = $monthend;
-    $inter = (int)$tri;
-    $tmp = getMonthMin($yearmax,$monthend,$inter-1);
-    $monthmin = $tmp[0];
-    $yearmin = $tmp[1];
-    $year = $yearmin;
-    $day = 1;
-    $hour = 0;
-    $min = 0;
-    $sec = 0;
-
-    //Début du traitement des infos
-
-    //Initialisation d'un tableau qui contiendra les num siren et leur valeurs associé le tout associer à un mois (1 les premiers, 2 le deuxième, etc...)
-    $annee = [];
-    for ($i = 1; $i <= $inter; $i++) {
-        $annee[$i] = [];
-    }
-
-    //On initialise un tableau qui gardera en mémoire les différents siren sur les derniers mois
-    $num_siren = array();
-
-    //traitement
-    for ($m = 1; $m != $inter+1; $m++){
-        //permet la borne entre le premiers mois et le suivant pour la requête sql
-        $tmp2 = addMonth($year,$monthmin,1);
-
-        $month = $tmp2[0];
-        $year = $tmp2[1];
-
-        //requête sql (utilisant make_timestamp pour pourvoir faire un between en utilisant des type timestamp)
-        $sql = "SELECT * FROM bank.impaye WHERE date_impaye BETWEEN make_timestamp($yearmin,$monthmin,$day,$hour,$min,$sec) AND make_timestamp($year,$month,$day,$hour,$min,$sec) ORDER BY date_impaye;";
-        $req = $cnx->query($sql);
-        $montant = 0;
-        while ($row = $req->fetch(PDO::FETCH_OBJ)) {
-           $siren = $row->num_siren;
-            if (!in_array($siren,$annee[$m])){
-                $annee[$m][$siren] = 0;
-            }
-            $annee[$m][$siren] += $row->montant; //Ajoute le montant
-             //Prend le siren associer à ce montant
-            if (!in_array($siren,$num_siren)) {array_push($num_siren,$siren);} //Ajoute le siren dans liste des sirens (si il y est pas déjà)
+    if ($datemax[0] != null) {
+        $yearmax = substr($datemax[0], 0, 4);
+        $yearmin = $yearmax;
+        $monthend = substr($datemax[0], 5, 2);
+        $monthmin = $monthend;
+        if ($date_begin == null && $date_end != null){
+            $inter = compareMonth($monthend,$month_end);
+        } else if ($date_begin != null && $date_end != null){
+            $inter = compareMonth($month_begin,$month_end);
+        } else {
+            $inter = (int)$tri;
         }
-        // On augmente la borne de 1 mois
-        $monthmin = $month;
-        $yearmin = $year;
+        print_r($inter);
+        $tmp = getMonthMin($yearmax, $monthend, $inter - 1);
+        $monthmin = $tmp[0];
+        $yearmin = $tmp[1];
+        $year = $yearmin;
+        $day = 1;
+        $hour = 0;
+        $min = 0;
+        $sec = 0;
+
+
+        //Début du traitement des infos
+
+        //Initialisation d'un tableau qui contiendra les num siren et leur valeurs associé le tout associer à un mois (1 les premiers, 2 le deuxième, etc...)
+        $annee = [];
+        for ($i = 1; $i <= $inter; $i++) {
+            $annee[$i] = [];
+        }
+
+        //On initialise un tableau qui gardera en mémoire les différents siren sur les derniers mois
+        $num_siren = array();
+
+        //traitement
+        for ($m = 1; $m != $inter + 1; $m++) {
+            //permet la borne entre le premiers mois et le suivant pour la requête sql
+            $tmp2 = addMonth($year, $monthmin, 1);
+
+            $month = $tmp2[0];
+            $year = $tmp2[1];
+
+            //requête sql (utilisant make_timestamp pour pourvoir faire un between en utilisant des type timestamp)
+            $sql = "SELECT * FROM bank.impaye WHERE date_impaye BETWEEN make_timestamp($yearmin,$monthmin,$day,$hour,$min,$sec) AND make_timestamp($year,$month,$day,$hour,$min,$sec) ORDER BY date_impaye;";
+            $req = $cnx->query($sql);
+            $montant = 0;
+            while ($row = $req->fetch(PDO::FETCH_OBJ)) {
+                $siren = $row->num_siren;
+                if (!in_array($siren, $annee[$m])) {
+                    $annee[$m][$siren] = 0;
+                }
+                $annee[$m][$siren] += $row->montant; //Ajoute le montant
+                //Prend le siren associer à ce montant
+                if (!in_array($siren, $num_siren)) {
+                    array_push($num_siren, $siren);
+                } //Ajoute le siren dans liste des sirens (si il y est pas déjà)
+            }
+            // On augmente la borne de 1 mois
+            $monthmin = $month;
+            $yearmin = $year;
+        }
+    } else {
+        echo "<div class='error'> There's no data to show or you selected the same date</div>";
     }
 ?>
   <script>
@@ -278,3 +347,4 @@ include("../include/po_navbar.inc.php"); // Navbar
 </body>
 
 </html>
+
