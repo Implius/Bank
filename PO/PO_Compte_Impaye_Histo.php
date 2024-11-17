499ea14f4be0411708624197fb79780b7f4322fc
GuillaumeAugeraud <augeraud.guillaume@gmail.com>
2024-11-17 16:25:16 +0100

    Update PO_Compte_Impaye_Histo.php

--- PO/PO_Compte_Impaye_Histo.php
+++ PO/PO_Compte_Impaye_Histo.php
@@ -42,10 +42,7 @@ if (isset($_POST["date_end"])) {
     <div class="mini_onit">Histogramme</div>
     <a class="mini_link" href="PO_Compte_Impaye_Circulaire.php">Circulaire</a>
 </div>
-<div class="canva">
-    <canvas id="mixedChart" style="width:100%;max-width:1200px;background-color: rgb(252, 248, 244);"></canvas>
-</div>
-
+<div class="Compte_histo">
 <div class="sorting">
     <div class="month">
         Trier par :
@@ -82,6 +79,11 @@ if (isset($_POST["date_end"])) {
         </form>
     </div>
 </div>
+</div>
+<div class="canva">
+    <canvas id="mixedChart" style="width:100%;max-width:1200px;background-color: rgb(252, 248, 244);"></canvas>
+</div>
+
 
 <?php
 global $cnx;
