8a46d7b2a14ccd8cff423281514f6e5b88c9d1cc
Implius <benjamin.rissot@yahoo.fr>
2024-11-18 20:32:54 +0100

    Update PO_Compte_Impaye_Circulaire.php

--- PO/PO_Compte_Impaye_Circulaire.php
+++ PO/PO_Compte_Impaye_Circulaire.php
@@ -9,6 +9,7 @@ include('../include/verifyconnexion.inc.php');
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.3/html2pdf.bundle.min.js"></script>
     <link rel="stylesheet" href="po_compte.css">
     <title>JeFinance</title>
 </head>
@@ -37,6 +38,9 @@ while ($ligne = $req->fetch(PDO::FETCH_OBJ)) {
     $code[$ligne->code_motif] += $ligne->montant;
 }
 ?>
+<div class="button_tel">
+    <button id="btn_pdf">Exporter format PDF</button>
+</div>
 <script>
     const ctx = document.getElementById('myChart').getContext('2d');
     const xValues = ["Code 01", "Code 02", "Code 03", "Code 04", "Code 05", "Code 06", "Code 07", "Code 08"];
@@ -123,6 +127,26 @@ while ($ligne = $req->fetch(PDO::FETCH_OBJ)) {
                 }
             }
         }
+    });
+</script>
+<script>
+    document.getElementById('btn_pdf').addEventListener('click', () => {
+        const element = document.getElementById('myChart');
+
+        // Obtenir les dimensions avec getBoundingClientRect
+        const rect = element.getBoundingClientRect();
+        const contentWidth = rect.width;
+        const contentHeight = rect.height;
+
+        const opt = {
+            margin: 0, //pas de marge
+            filename: 'table.pdf',
+            image: { type: 'jpeg', quality: 0.98 },
+            html2canvas: { scale: 2, width: contentWidth, height: contentHeight }, // Meilleure qualité
+            jsPDF: { unit: 'px', format: [contentWidth, contentHeight], orientation: 'landscape' }
+        };
+        // Génération du PDF
+        html2pdf().set(opt).from(element).save();
     });
 </script>
 </body>
