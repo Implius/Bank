4d971b819563844e2a14803cf05b2fa8d52c917d
Andrei Achirecesei <90113072+mickeyilestmignon@users.noreply.github.com>
2024-11-18 18:35:59 +0100

    bloquage 3 tentatives

--- po_login.php
+++ po_login.php
@@ -1,52 +1,40 @@
-<?php
-if (!isset($_COOKIE['tentatives'])) {
-    setcookie('tentatives', 0, time() + 60*60*60, '/');
-}
-else {
-    if (isset($_GET['error'])) {
-        if ($_GET['error'] == 1) {
-            setcookie('tentatives', $_COOKIE['tentatives'] + 1, time() + 60*60*60, '/');
-        }
-    }
-}
-?>
 <!doctype html>
 <html lang="en">
-<head>
+  <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="po_login.css">
     <title>JeFinance</title>
-</head>
-<body>
-<div class="Large">
+  </head>
+  <body>
+    <div class="Large">
     <div class="connectcard" className="items-center">
 
         <form action="login_check.php" method="post">
-            <input class="idconnexion" type="text" id="NumSiren" name="NumSiren" placeholder="Numéro de SIREN" required
-                <?php
-                if (isset($_COOKIE['tentatives'])) {
-                    if ($_COOKIE['tentatives'] >= 3) {
-                        echo "disabled";
-                    }
-                }
-                ?>
-            ><br>
-            <input class="passwordconnexion" type="password" id="password" name="password" placeholder="Mot de passe" required
-                <?php
-                if (isset($_COOKIE['tentatives'])) {
-                    if ($_COOKIE['tentatives'] >= 3) {
-                        echo "disabled";
-                    }
-                }
-                ?>
-            >
+          <input class="idconnexion" type="text" id="NumSiren" name="NumSiren" placeholder="Numéro de SIREN" required
+              <?php
+              if (isset($_COOKIE['tentatives'])) {
+                  if ($_COOKIE['tentatives'] >= 3) {
+                      echo "disabled";
+                  }
+              }
+              ?>
+          ><br>
+          <input class="passwordconnexion" type="password" id="password" name="password" placeholder="Mot de passe" required
+              <?php
+              if (isset($_COOKIE['tentatives'])) {
+                  if ($_COOKIE['tentatives'] >= 3) {
+                      echo "disabled";
+                  }
+              }
+              ?>
+          >
             <label class="checkbox-container">
-                <input class="hidden" type="checkbox" onclick="myFunction()">
-                <span class="checkmark"></span>
-            </label>
-            <br>
-            <button class="buttonconnexion" type="submit">Se connecter</button>
+              <input class="hidden" type="checkbox" onclick="myFunction()">
+              <span class="checkmark"></span>
+          </label>
+          <br>
+          <button class="buttonconnexion" type="submit">Se connecter</button>
         </form>
 
         <?php
@@ -55,6 +43,9 @@ else {
                 echo "<p class='error'>Erreur d'authentification</p>";
                 if ($_COOKIE['tentatives'] == 2) {
                     echo "<p class='error'>ATTENTION : C'est votre dernier essai</p>";
+                }
+                else if ($_COOKIE['tentatives'] >= 3) {
+                    echo "<p class='error'>Nombre de tentatives épuisé, réessayez dans 1 heure</p>";
                 }
             }
             elseif ($_GET['error'] == 2) {
@@ -67,18 +58,18 @@ else {
         ?>
 
     </div>
-</div>
+  </div>
 
-<script>
-    function myFunction() {
-        var x = document.getElementById("password");
-        if (x.type === "password") {
-            x.type = "text";
-        } else {
-            x.type = "password";
-        }
+  <script>
+      function myFunction() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
     }
-</script>
+  }
+  </script>
 
-</body>
+  </body>
 </html>
