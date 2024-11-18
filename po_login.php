996020107a3a09decf2f8f8b8266f712e4ee2123
Andrei Achirecesei <90113072+mickeyilestmignon@users.noreply.github.com>
2024-11-18 18:21:40 +0100

    Update po_login.php

--- po_login.php
+++ po_login.php
@@ -1,3 +1,16 @@
+<?php
+if (!isset($_COOKIE['tentatives'])) {
+    setcookie('tentatives', 3, time() + 60*60*60, '/');
+}
+else {
+    if (isset($_GET['error'])) {
+        if ($_GET['error'] == 1) {
+            setcookie('tentatives', $_COOKIE['tentatives'] + 1, time() + 60*60*60, '/');
+        }
+    }
+}
+echo $_COOKIE['tentatives'];
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -11,8 +24,24 @@
     <div class="connectcard" className="items-center">
 
         <form action="login_check.php" method="post">
-          <input class="idconnexion" type="text" id="NumSiren" name="NumSiren" placeholder="Numéro de SIREN" required><br>
-          <input class="passwordconnexion" type="password" id="password" name="password" placeholder="Mot de passe" required>
+          <input class="idconnexion" type="text" id="NumSiren" name="NumSiren" placeholder="Numéro de SIREN" required
+              <?php
+              if (isset($_COOKIE['tentatives'])) {
+                  if ($_COOKIE['tentatives'] >= 3) {
+                      echo "disabled";
+                  }
+              }
+              ?>
+          ><br>
+          <input class="passwordconnexion" type="password" id="password" name="password" placeholder="Mot de passe" required
+              <?php
+              if (isset($_COOKIE['tentatives'])) {
+                  if ($_COOKIE['tentatives'] >= 3) {
+                      echo "disabled";
+                  }
+              }
+              ?>
+          >
             <label class="checkbox-container">
               <input class="hidden" type="checkbox" onclick="myFunction()">
               <span class="checkmark"></span>
@@ -25,6 +54,9 @@
         if (isset($_GET['error'])) {
             if ($_GET['error'] == 1) {
                 echo "<p class='error'>Erreur d'authentification</p>";
+                if ($_COOKIE['tentatives'] == 2) {
+                    echo "<p class='error'>ATTENTION : C'est votre dernier essai</p>";
+                }
             }
             elseif ($_GET['error'] == 2) {
                 echo "<p class='error'>Vous n'avez pas les droits nécessaires</p>";
