6926fffc8a1d5c1b5ffecef0354b321c9f36b3d9
Implius <benjamin.rissot@yahoo.fr>
2024-11-29 16:23:18 +0100

    Update export_compte.php ajout comms

--- PO/export_compte.php
+++ PO/export_compte.php
@@ -1,14 +1,21 @@
 <?php
+//Recupere les donnees (au format json)
 $data = json_decode(file_get_contents('php://input'),true);
+
+//des options du ficher
 header('Content-Type: text/csv');
 header("Content-Disposition: attachement; filename='export.csv'");
 
+// Ouvre le fichier en mode write
 $ouput = fopen("php://output", "w");
 
+//Entre la ligne de titres
 fputcsv($ouput, array('Raison social','N° Compte','N° Siren','Montant'),";");
 
+//Entre dans le fichier les donnees recuperees
 foreach ($data as $row) {
     fputcsv($ouput, $row, ";");
 }
 
+//ferme le fichier
 fclose($ouput);
