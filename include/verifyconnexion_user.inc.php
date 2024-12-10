f6bb43bada32007c5db3c7e97c8c6e03de113626
Andrei Achirecesei <90113072+mickeyilestmignon@users.noreply.github.com>
2024-12-10 14:35:48 +0100

    check user is not po or admin

--- include/verifyconnexion_user.inc.php
+++ include/verifyconnexion_user.inc.php
@@ -20,3 +20,17 @@ if (!$ligne) {
     header('location: ../po_login.php?error=3'); // Si l'utilisateur n'existe pas
 
 }
+
+$req_po = $cnx->prepare("SELECT * FROM po WHERE id_util=:id_util");
+$req_po->bindParam(':id_util', $ligne->id_util);
+$req_po->execute();
+$ligne_po=$req_po->fetch(PDO::FETCH_OBJ);
+
+$req_admin = $cnx->prepare("SELECT * FROM admin WHERE id_util=:id_util");
+$req_admin->bindParam(':id_util', $ligne->id_util);
+$req_admin->execute();
+$ligne_admin=$req_admin->fetch(PDO::FETCH_OBJ);
+
+if ($ligne_admin || $ligne_po) {
+    header('location: ../po_login.php?error=3'); // Si l'utilisateur est un admin ou un po
+}
