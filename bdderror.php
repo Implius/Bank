bf444841eb94c321ac6765bb67d9367028e3e333
Andrei Achirecesei <90113072+mickeyilestmignon@users.noreply.github.com>
2024-12-10 14:23:17 +0100

    bdd error

---
+++ bdderror.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="po_login.css">
+    <title>JeFinance</title>
+</head>
+<body>
+
+<div class="bdderror">
+    Une erreur de connexion à la base de données s'est produite. Veuillez réessayer plus tard.<br><a href="po_login.php">Retour à la page de connexion</a>
+</div>
+
+</body>
+</html>
