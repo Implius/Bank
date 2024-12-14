76870fcd0c76e6ea35b5140ee23b136a4d28d535
GuillaumeAugeraud <augeraud.guillaume@gmail.com>
2024-12-14 16:15:47 +0100

    Update PO_impaye_Circu.php
    Changement du diagramme circulaire

--- PO/PO_impaye_Circu.php
+++ PO/PO_impaye_Circu.php
@@ -56,99 +56,122 @@ include('../include/verifyconnexion.inc.php');
   <div class="button_tel">
       <button id="btn_pdf">Exporter format PDF</button>
   </div>
+ <?php
+  global$cnx;
+  include("../include/connexion.inc.php");
+  //initialise un tableaux contenant les codes et associe un montant (0 au depart)
+  $code = [
+          "01" => 0, "02" => 0, "03" => 0, "04" => 0, "05" => 0, "06" => 0, "07" => 0, "08" => 0,
+  ];
+
+  //recupere les montant associer aux differents codes et les ajoutes a notre tableau
+  $sql = "SELECT code_motif,montant FROM impaye;";
+  $req = $cnx->query("SELECT code_motif,montant FROM impaye;");
+  while ($ligne = $req->fetch(PDO::FETCH_OBJ)) {
+      $code[$ligne->code_motif] += $ligne->montant;
+  }
+
+  ?>
+  <div class="button_tel">
+      <button id="btn_pdf">Exporter format PDF</button>
+  </div>
   <script>
     //definit les options et les valeurs du graphique
     const ctx = document.getElementById('myChart').getContext('2d');
-    const xValues = ["Code 01", "Code 02", "Code 03", "Code 04", "Code 05", "Code 06", "Code 07", "Code 08"];
-    const yValues = [<?php echo $code["01"] . "," . $code["02"] . "," . $code["03"] . "," . $code["04"] . "," . $code["05"] . "," . $code["06"] . "," . $code["07"] . "," . $code["08"]; ?>];
-    
+    const xValues = ["Fraude à la carte", "Compte à découvert", "Compte clôturé", "Compte bloqué", "Provision insuffisante", "Opération contestée par le débiteur", "Titulaire décédé", "Raison non communiquée"];
+    const yValues = [<?php echo implode(',', array_values($code)); ?>];
+
     function createRandomGradient() {
-      //Permet de cree une couleur aleatoire en format RGB
-      const color1 = barColors[Math.floor(Math.random() * barColors.length)];
-      const color2 = barColors[Math.floor(Math.random() * barColors.length)];
-      const gradient = ctx.createLinearGradient(0, 0, 0, 400);
-      gradient.addColorStop(0, color1);
-      gradient.addColorStop(1, color2);
-      return gradient;
+        const color1 = barColors[Math.floor(Math.random() * barColors.length)];
+        const color2 = barColors[Math.floor(Math.random() * barColors.length)];
+        const gradient = ctx.createLinearGradient(0, 0, 0, 400);
+        gradient.addColorStop(0, color1);
+        gradient.addColorStop(1, color2);
+        return gradient;
     }
-
     function createRadialGradient(color1, color2) {
-            //Permet de cree un degrade entre deux couleur
-            const gradient = ctx.createLinearGradient(200, 200, 50, 200, 200, 200);
-            gradient.addColorStop(0, color1);
-            gradient.addColorStop(1, color2);
-            return gradient;
-        }
+        const gradient = ctx.createLinearGradient(200, 200, 50, 200, 200, 200);
+        gradient.addColorStop(0, color1);
+        gradient.addColorStop(1, color2);
+        return gradient;
+    }
     const barColors = [
-      "#FF0000",
-      "#FF4500",
-      "#FF7F00",
-      "#FFB300",
-      "#FFFF00",
-      "#B3FF00",
-      "#00FF00",
-      "#00FF7F",
-      "#00FFFF",
-      "#007FFF",
-      "#0000FF",
-      "#4500FF",
-      "#8000FF",
-      "#FF00FF",
-      "#FF007F",
-      "#FF9999",
-      "#FFCC99",
-      "#FFFF99",
-      "#99FF99",
-      "#99FFFF",
-      "#9999FF",
-      "#D699FF",
+        "#FF0000",
+        "#FF4500",
+        "#FF7F00",
+        "#FFB300",
+        "#FFFF00",
+        "#B3FF00",
+        "#00FF00",
+        "#00FF7F",
+        "#00FFFF",
+        "#007FFF",
+        "#0000FF",
+        "#4500FF",
+        "#8000FF",
+        "#FF00FF",
+        "#FF007F",
+        "#FF9999",
+        "#FFCC99",
+        "#FFFF99",
+        "#99FF99",
+        "#99FFFF",
+        "#9999FF",
+        "#D699FF",
     ];
-
+    const gradients = [
+        createRadialGradient("#F44336", "#E81E63"),
+        createRadialGradient("#9c27b0", "#673ab7"),
+        createRadialGradient("#3f51b5", "#2196f3"),
+        createRadialGradient("#03a9f4", "#00bcd4"),
+        createRadialGradient("#009688", "#4caf50"),
+        createRadialGradient("#8BC34A", "#cddc39"),
+        createRadialGradient("#ffeb3b", "#ffc107"),
+        createRadialGradient("#ff9800", "#ff5722")
+    ];
     new Chart(ctx, {
-      type: "pie",
-      data: {
-        labels: xValues,
-        datasets: [{
-          backgroundColor: [
-          createRadialGradient("#F44336", "#E81E63"),  // Coral to Light Orange to Light Yellow Orange
-    createRadialGradient("#9c27b0", "#673ab7"),  // Light Blue to Sky Blue to Dark Blue
-    createRadialGradient("#3f51b5", "#2196f3"),  // Purple to Light Purple to Light Pink Purple
-    createRadialGradient("#03a9f4", "#00bcd4"),  // Light Red to Light Orange to Light Yellow Orange
-    createRadialGradient("#009688", "#4caf50"),  // Teal to Light Teal to Light Green Teal
-    createRadialGradient("#8BC34A", "#cddc39"),  // Light Yellow to Golden Yellow to Dark Yellow
-    createRadialGradient("#ffeb3b", "#ffc107"),  // Indigo to Light Indigo to Dark Indigo
-    createRadialGradient("#ff9800", "#ff5722")
-          ],
-          data: yValues,
-          borderWidth: 0,
-        }]
-
-      },
-      options: {
-        responsive: false,
-        title: {
-          display: true,
-          text: "World Wide Wine Production 2018"
+        type: "pie",
+        data: {
+            labels: xValues,
+            datasets: [{
+                data: yValues,
+                backgroundColor: gradients,
+                borderWidth: 0,
+            }]
         },
-        plugins: {
-          tooltip: {
-            bodyFont: {
-              size: 25 // Change the font size for tooltip body
+        options: {
+            responsive: false,
+            title: {
+                display: true,
+                text: "Impayé"
             },
-            titleFont: {
-              size: 25 // Change the font size for tooltip title
-            },
-          },
-          legend: {
-            labels: {
-              color: 'black',
-              font: {
-                size: 19 // Change the font size for legend labels
-              }
+            plugins: {
+                tooltip: {
+                    callbacks: {
+                        label: (context) => {
+                            const value = context.raw;
+                            const total = context.dataset.data.reduce((sum, val) => sum + val, 0);
+                            const percentage = ((value / total) * 100).toFixed(1);
+                            return `${value}<?php echo $devise ?>(${percentage}%)`;
+                        }
+                    },
+                    bodyFont: {
+                        size: 25 // Change the font size for tooltip body
+                    },
+                    titleFont: {
+                        size: 25 // Change the font size for tooltip title
+                    },
+                },
+                legend: {
+                    labels: {
+                        color: 'black',
+                        font: {
+                            size: 19 // Change the font size for legend labels
+                        }
+                    }
+                },
             }
-          }
         }
-      }
     });
   </script>
   <script>
