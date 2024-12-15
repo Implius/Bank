812d6ea2098cb3dd4e7757350ba379de7dbd121f
Jules <julesgrange@outlook.fr>
2024-12-15 16:59:17 +0100

    - not showing up in the list

--- PO/po_compte.php
+++ PO/po_compte.php
@@ -37,7 +37,6 @@ if (isset($_SESSION['num_siren'])) {
         }
     </script>
 </head>
-²
 <body>
 
 <?php
@@ -169,7 +168,7 @@ if (!isset($_GET['search']) || $_GET['search'] == "") {
                             break;
                     }
                     if ($ligne->sens=='-'){
-                        echo "<p class='red'>$ligne->tresorerie $devise</p>";
+                        echo "<p class='red'>- $ligne->tresorerie $devise</p>";
                     } else {
                     echo "<p>$ligne->tresorerie $devise</p>"; }?>
                 </td>
